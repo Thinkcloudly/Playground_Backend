18dcd602293175185f1d33d426c0b0a696c26b61
Anirban Kar <thecodacus@gmail.com>
2022-08-09 13:31:30 +0530

    scenario completed for poc

--- src/controllers/validations/scenario1.controller.ts
+++ src/controllers/validations/scenario1.controller.ts
@@ -1,6 +1,6 @@
 import { DescribeStackResourcesCommand, DescribeStackResourcesCommandInput } from '@aws-sdk/client-cloudformation';
 import express, { Router, Request, Response } from 'express';
-
+import { DescribeVolumesCommand, DescribeVolumesCommandInput, EC2Client } from '@aws-sdk/client-ec2'
 let router = Router();
 
 router.post('/validate-scenario-1', async (req: Request, res: Response) => {
@@ -20,8 +20,49 @@ router.post('/validate-scenario-1', async (req: Request, res: Response) => {
         //     console.log(element.LogicalResourceId);
 
         // });
+        let Resources: any[] = data.StackResources.map(element => {
+            return {
+                LogicalResourceId: element.LogicalResourceId,
+                PhysicalResourceId: element.PhysicalResourceId,
+                ResourceType: element.ResourceType,
+                Timestamp: element.Timestamp,
+                ResourceStatus: element.ResourceStatus,
+            }
+        })
 
-        res.send({ ...data });
+        let ec2Id = Resources.find(x => x.ResourceType === "AWS::EC2::Instance")
+        let ebsId = Resources.find(x => x.ResourceType === "AWS::EC2::Volume")
+
+        let ec2Client = new EC2Client({
+            "credentials": {
+                accessKeyId: `${process.env.AWS_ID}`,
+                secretAccessKey: `${process.env.AWS_SECRET}`
+            },
+            region: `${process.env.AWS_REGION}`
+        });
+
+        let scParams: DescribeVolumesCommandInput = {
+            MaxResults: 10,
+            Filters: [
+                {
+                    "Name": "attachment.instance-id",
+                    "Values": [ec2Id.PhysicalResourceId]
+                }
+            ]
+        }
+        let scCmd = new DescribeVolumesCommand(scParams)
+        let results = await ec2Client.send(scCmd);
+        if (results.Volumes.find(x => x.VolumeId == ebsId.PhysicalResourceId)) {
+            return res.send({
+                status: "success",
+                message: "scenario successfully completed"
+            })
+        }
+
+        return res.send({
+            status: "failed",
+            message: "scenario not completed"
+        })
         // process data.
     } catch (error: any) {
         // error handling.
