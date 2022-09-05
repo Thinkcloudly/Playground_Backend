a28c3cb184703467b31eb508a48a241a7e3f13f9
Anirban Kar <thecodacus@gmail.com>
2022-09-05 21:57:24 +0530

    swagger added

--- src/api.ts
+++ src/api.ts
@@ -6,6 +6,9 @@ import { environmentController } from './controllers/environment.controller';
 import { CloudFormationClient } from '@aws-sdk/client-cloudformation';
 import serverless from 'serverless-http'
 
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 dotenv.config();
 
 const app: Express = express();
@@ -18,6 +21,10 @@ app.use(json())
 app.use(cors({
   origin: '*'
 }))
+
+
+
+
 app.use('./', (req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -27,6 +34,20 @@ app.use('./', (req, res, next) => {
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+// setting up swaggger
+const openapiSpecification = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'ThinkCloudly Playground App',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./controllers/*.js'], // files containing annotations as above
+});
+
+app.use('/.netlify/functions/api/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.use('/.netlify/functions/api', (req: Request, res: Response, next) => {
   req.awsClient = new CloudFormationClient({
