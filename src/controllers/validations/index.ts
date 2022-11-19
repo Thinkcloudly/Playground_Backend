9bb0291f6d133b905795a6c5533c9cd2ec66a2f2
Anirban Kar <thecodacus@gmail.com>
2022-11-20 00:57:27 +0530

    added scenario 3

--- src/controllers/validations/index.ts
+++ src/controllers/validations/index.ts
@@ -1,8 +1,11 @@
 import express, { Router, Request, Response } from 'express';
 import scenario1Controller from './scenario1.controller';
 import scenario2Controller from './scenario2.controller';
+import scenario3Controller from './scenario3.controller';
+
 let router = Router();
 router.use("/testEnv1", scenario1Controller);
 router.use("/testEnv2", scenario2Controller);
+router.use("/testEnv3", scenario3Controller);
 export default router
 export const scenarioControllers = router;
---
+++ src/controllers/validations/scenario3.controller.ts
@@ -0,0 +1,83 @@
+import { DescribeStackResourcesCommand, DescribeStackResourcesCommandInput } from '@aws-sdk/client-cloudformation';
+import { S3Client, AbortMultipartUploadCommand, GetBucketPolicyCommand, GetBucketPolicyCommandInput } from "@aws-sdk/client-s3";
+import express, { Router, Request, Response } from 'express';
+let router = Router();
+
+router.post('/', async (req: Request, res: Response) => {
+    let { stackId } = req.body;
+    if (!stackId) {
+        res.status(400).send('Missing environment or userId');
+        return;
+    }
+    const params: DescribeStackResourcesCommandInput = {
+        StackName: `${stackId}`
+    };
+    const command = new DescribeStackResourcesCommand(params);
+    // async/await.
+    try {
+        const data = await req.awsClient.send(command);
+        // data.StackResources.forEach(element => {
+        //     console.log(element.LogicalResourceId);
+
+        // });
+        // console.log(data);
+        // return res.send(data);
+
+        let Resources: any[] = data.StackResources.map(element => {
+            return {
+                LogicalResourceId: element.LogicalResourceId,
+                PhysicalResourceId: element.PhysicalResourceId,
+                ResourceType: element.ResourceType,
+                Timestamp: element.Timestamp,
+                ResourceStatus: element.ResourceStatus,
+            }
+        })
+
+        let resId = Resources.find(x => x.ResourceType === "AWS::S3::Bucket")
+        console.log("found res");
+
+        console.log(resId);
+
+        let s3Client = new S3Client({
+            "credentials": {
+                accessKeyId: `${process.env.AWS_ID}`,
+                secretAccessKey: `${process.env.AWS_SECRET}`
+            },
+            region: `${process.env.AWS_ENV_REGION}`
+        });
+        let s3PolicyInput: GetBucketPolicyCommandInput = {
+            Bucket: resId.PhysicalResourceId
+        }
+        const s3Command = new GetBucketPolicyCommand(s3PolicyInput);
+        let s3Resp = await s3Client.send(s3Command);
+        console.log(s3Resp);
+        if (s3Resp.Policy !== undefined) {
+            return res.send({
+                status: "success",
+                message: "scenario completed",
+            })
+        }
+        else {
+            return res.send({
+                status: "failed",
+                message: "scenario not completed",
+
+            })
+        }
+
+
+        // process data.
+    } catch (error: any) {
+        // error handling.
+        console.log(error);
+        /**
+         * The keys within exceptions are also parsed.
+         * You can access them by specifying exception names:
+         * if (error.name === 'SomeServiceException') {
+         * 
+         **/
+        res.status(500).send({ ...error })
+    }
+});
+export default router;
+export const scenario1Controller = router;
