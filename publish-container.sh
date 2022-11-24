c7f0d0d4ec77bcec25b0d9ba0e154de70d050b3a
Anirban Kar <thecodacus@gmail.com>
2022-11-24 15:34:04 +0530

    k8s

--- publish-container.sh
+++ publish-container.sh
@@ -1,4 +1,4 @@
 docker build -t playground-backend . --platform linux/amd64     
-docker image rm 071688540161.dkr.ecr.us-east-1.amazonaws.com/playground-backend:latest  
-docker tag playground-backend:latest 071688540161.dkr.ecr.us-east-1.amazonaws.com/playground-backend:latest
-docker push 071688540161.dkr.ecr.us-east-1.amazonaws.com/playground-backend:latest
+docker image rm 654202376781.dkr.ecr.us-east-1.amazonaws.com/playground-backend:latest  
+docker tag playground-backend:latest 654202376781.dkr.ecr.us-east-1.amazonaws.com/playground-backend:latest
+docker push 654202376781.dkr.ecr.us-east-1.amazonaws.com/playground-backend:latest
